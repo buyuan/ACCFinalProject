b3c781bac1a6b65abb3471067f1f66f0d578b4b7
BuyuanCai <neobuyuan@gmail.com>
2021-11-18 09:47:13 +0800

    update crawl code

--- src/WebCrawler/Attribute.java
+++ src/WebCrawler/Attribute.java
@@ -2,8 +2,14 @@ package WebCrawler;
 
 public class Attribute {
 	private static final String outputPath = "E:/UW/ACC/FinalProject/Data/";
+	public static final String baseWeb = "https://www.imdb.com";
 	
 	public static String getOutputPath(){
 		return outputPath;
+	}
+	
+	
+	public static String getBaseWeb(){
+		return baseWeb;
 	}
 }
--- src/WebCrawler/WebCrawler.java
+++ src/WebCrawler/WebCrawler.java
@@ -4,10 +4,15 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.safety.Whitelist;
 import org.jsoup.select.Elements;
 
 public class WebCrawler {
@@ -18,34 +23,93 @@ public class WebCrawler {
 	 * @throws IOException 
 	 *
 	 */
-	public static String crawlOnePage(String url) throws IOException {
-		Document doc =  Jsoup.connect(url).get();
-		String text  = doc.text();
-		String title = doc.title();
-		String path = Attribute.getOutputPath();
+	public static final String path =Attribute.getOutputPath();	
+	public static final String baseWeb = Attribute.getBaseWeb(); //The target web
+	public static Map<String, Boolean> badlinks = new HashMap<>();//for storing the links that cannot be fetched when crawling
+	public static Set<String> badLinks = new HashSet<>();
+	
+	public static void searchEngine() throws IOException{
+		Map<String, Boolean> alllinks = new HashMap<>();//for store all the links that are to be search.put(baseWeb, false);
+		alllinks.put(baseWeb,false);
+		alllinks = crawlPage(alllinks);
 		
-		storing(text,title,url,path);
-		String links="";
-		Elements ele = doc.select("a");
-		for(Element e2:ele) {
-        	String href = e2.attr("abs:href");
-        	if(href.length()>3) {
-        		links = links+"\n"+href;        	
-        	}
-        }
-		return links;
+		//Store the url into one file
+		File linkList = new File(path+"weblist/webLinkList.txt");
+		try {     	  
+ 			// if file does not exists, then create it
+ 			if (!linkList.exists()) {
+ 				linkList.createNewFile();
+ 			}
+ 			FileWriter fw = new FileWriter(linkList.getAbsoluteFile());
+ 			BufferedWriter bw = new BufferedWriter(fw);
+ 			for(Map.Entry<String, Boolean> entry: alllinks.entrySet()) {
+ 				bw.write(entry.getKey());
+ 			}
+ 			bw.close();				
+ 		}catch (IOException e) {
+	     	   e.printStackTrace();
+	     	  }	
+		
+		// store bad links
+		File badUrls = new File(path+"badweblist/badwebLinkList.txt");
+		try {     	  
+ 			// if file does not exists, then create it
+ 			if (!badUrls.exists()) {
+ 				badUrls.createNewFile();
+ 			}
+ 			FileWriter fw = new FileWriter(badUrls.getAbsoluteFile());
+ 			BufferedWriter bw = new BufferedWriter(fw);
+ 			for(String s: badLinks) {
+ 				bw.write(s);
+ 			}
+ 			bw.close();				
+ 		}catch (IOException e) {
+	     	   e.printStackTrace();
+	     	  }	
 	}
-	public static void makeTheDataBase(String url) throws IOException{
-		String allLinks = crawlOnePage(url);
-		String[] links = allLinks.split("\n");
-		for(String lk:links) {
-			System.out.println(lk);
+	public static Map<String, Boolean> crawlPage(Map<String, Boolean> mp) throws IOException {
+		System.out.println("Processing-----------------------");
+		Map<String, Boolean> newLinkList = new HashMap<>();//Links of the input link
+		String curLink = "";
+		for(Map.Entry<String, Boolean> link : mp.entrySet()){
+			if(!link.getValue()) {//the link is not crawled
+				curLink = link.getKey();
+				try {
+					Document doc =  Jsoup.connect(curLink).timeout(5000).get();
+					String text  = doc.text();
+					String title = doc.title();	
+					//store the data into a file
+					storingInFile(text,title,curLink,path);
+					//find new link
+					Elements ele = doc.select("a");
+					for(Element e2:ele) {
+			        	String href = e2.attr("abs:href");
+			        	//not enpty, not find before, and it belongs to baseweb
+			        	if(!href.isEmpty()&&!mp.containsKey(href)&&
+			        			!newLinkList.containsKey(href)&&(href.startsWith(baseWeb))) {
+			        		newLinkList.put(href, false);
+			        	}
+			        }	
+				}catch (IOException e) {
+					   mp.replace(curLink,false,true);//Set bad link processed
+					   badLinks.add(curLink);
+					   e.printStackTrace();
+		 			   continue;   
+			     	  }
+			}
+			mp.replace(curLink,false,true);//set the link as ture, means the li nk is processed
 		}
-		
-		
+		if(!newLinkList.isEmpty()) {
+			mp.putAll(newLinkList);//merge the new links into the allLinkList
+			Map<String, Boolean> tempLinks = crawlPage(mp);
+			mp.putAll(tempLinks);
+		}
+		return mp;
 	}
-	private static void storing(String text, String title, String url, String path) {
- 		File temp = new File(path+title+".txt");
+
+	private static void storingInFile(String text, String title, String url, String path) {
+		String fixedTitle = adjustChar(title);
+ 		File temp = new File(path+fixedTitle+".txt");
  		try {     	  
  			// if file does not exists, then create it
  			if (!temp.exists()) {
@@ -62,11 +126,34 @@ public class WebCrawler {
  			bw.write(text);
  			bw.close();	
  		}catch (IOException e) {
+ 			   System.out.println(title);
 	     	   e.printStackTrace();
+	     	   
 	     	  }
 	}
+	public static String adjustChar(String str){
+		if(!str.contains("\\")&&!str.contains("/")&&!str.contains(":")&&
+				!str.contains("?")&&!str.contains("<")&&!str.contains(">")
+				&&!str.contains("|")&&!str.contains("\"")) {
+			return str;
+		}
+		char[] chararr = str.toCharArray();
+		for(int i=0;i<chararr.length;i++) {
+			chararr[i] = chargeChar(chararr[i]);
+		}
+		return new String(chararr);
+	}
+
+	private static char chargeChar(char c) {
+		if(c=='\\'||c=='/'||c==':'||c=='*'||c=='?'||c=='<'||c=='>'||c=='|'||c=='\"') {
+			return '-';
+		}else {
+			return c;
+		}
+		
+	}
 	public static void main(String[] args) throws IOException {
-		String url = "https://www.baidu.com";
-		makeTheDataBase(url);
+		//baseWeb = "https://www.imdb.com";
+		searchEngine();
 	}
 }		
